4b002a7e58bf2f0d7efc12582094a56393ab4b44
vicky1903 <50952787+vicky1903@users.noreply.github.com>
2019-06-05 23:07:08 +0200

    first try

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var e = document.getElementById('contener1');
+e.onmouseover = function() {
+  document.getElementById('text1').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('text1').style.display = 'none';
+}
